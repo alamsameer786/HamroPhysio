e540f705d304498d48855bb9c9543bb2eb87a3a6
alamsameer786 <s1010alam@gmail.com>
2026-05-16 23:19:36 +0545

    Update urls.py

--- hamrophysio/urls.py
+++ hamrophysio/urls.py
@@ -2,12 +2,17 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from core import views
+
+handler404 = 'core.views.custom_404'
+handler500 = 'core.views.custom_500'
 
 urlpatterns = [
-    path('admin/', admin.site.urls),  # Django built-in admin
-    path('', include('core.urls')),   # Your app URLs
+    path('admin/', admin.site.urls),
+    path('', include('core.urls')),
 ]
 
-if settings.DEBUG:
+# Serve static and media files in production
+if not settings.DEBUG:
     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
